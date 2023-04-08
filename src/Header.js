58be43a333c91f73f0a4dd5c85f56aade35f67f6
grumpycatyo-collab <77546227+grumpycatyo-collab@users.noreply.github.com>
2023-04-09 02:28:13 +0300

    Header #4
    Added list to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Grid, GridItem, Image, Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
+import { Grid, GridItem, Image, Input, InputGroup, InputRightElement, Button,UnorderedList, ListItem  } from "@chakra-ui/react";
+import "./Clickable.css"
 
 
 function Header() {
+    const handleClick = (item) => {
+        console.log(`You clicked on ${item}`);
+      };
+    const items = ["What is the process of voting?", "How would Rick explain the voting process in Moldova?",
+                "What would Maya Sandu do if she were a samurai?", "Why should I go to the vote?", "Where is the lottery?"]
   return (
     <Grid templateColumns="repeat(12, 1fr)" gap={4}  p={4}>
-      <GridItem colSpan={1}>
+      <GridItem colSpan={0}>
         <Image src={"https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg"} alt="Logo" />
       </GridItem>
       <GridItem colSpan={3}>
@@ -19,8 +25,14 @@ function Header() {
           </InputRightElement>
         </InputGroup>
       </GridItem>
-      <GridItem colSpan={2}>
-        
+      <GridItem colSpan={3}  >
+      <UnorderedList backgroundColor={"white"} style={{ listStyleType: "none" }}>
+      {items.map((item) => (
+        <ListItem key={item} onClick={() => handleClick(item)} className = "list-underline">
+          {item}
+        </ListItem>
+      ))}
+    </UnorderedList>
       </GridItem>
     </Grid>
   );
